484c7b306492875b027c3073a6b141bf4968054c
viralpsuzy <83786761+viralpsuzy@users.noreply.github.com>
2021-05-06 22:13:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Test Azure!"
-
-import pyodbc
-
-def test():
-    return "This is hosted web application, run using Python"
+    return "Hello, Test Azure! This is hosted web application, run using Python."
